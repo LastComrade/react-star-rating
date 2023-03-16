fdaf31a51a481753873804566c26babcd8fa1377
Konark Lohat <konarklohat123456@gmail.com>
2023-03-16 13:07:37 +0530

    Aman chutiya

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [ratedStar, setRatedStar] = useState(0);
+
+  const handleClick = (e) => {
+    setRatedStar(e.target.id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Star Rating</h1>
+      </div>
+      <div className="star-screen">
+        <div>
+          <span
+            className={`star ${
+              ratedStar.toString() === "1"
+                ? "ratedStar"
+                : ratedStar > 1
+                ? "lessRatedStar"
+                : "nonRatedStar"
+            }`}
+            id="1"
+            onClick={handleClick}
+          >
+            *
+          </span>
+          <span> </span>
+          <span
+            className={`star ${
+              ratedStar.toString() === "2"
+                ? "ratedStar"
+                : ratedStar > 2
+                ? "lessRatedStar"
+                : "nonRatedStar"
+            }`}
+            id="2"
+            onClick={handleClick}
+          >
+            *
+          </span>
+          <span> </span>
+          <span
+            className={`star ${
+              ratedStar.toString() === "3"
+                ? "ratedStar"
+                : ratedStar > 3
+                ? "lessRatedStar"
+                : "nonRatedStar"
+            }`}
+            id="3"
+            onClick={handleClick}
+          >
+            *
+          </span>
+          <span> </span>
+          <span
+            className={`star ${
+              ratedStar.toString() === "4"
+                ? "ratedStar"
+                : ratedStar > 4
+                ? "lessRatedStar"
+                : "nonRatedStar"
+            }`}
+            id="4"
+            onClick={handleClick}
+          >
+            *
+          </span>
+          <span> </span>
+          <span
+            className={`star ${
+              ratedStar.toString() === "5"
+                ? "ratedStar"
+                : ratedStar > 5
+                ? "lessRatedStar"
+                : "nonRatedStar"
+            }`}
+            id="5"
+            onClick={handleClick}
+          >
+            *
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
